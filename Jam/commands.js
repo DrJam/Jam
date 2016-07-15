b799e51cb7884a3c3b96bcf1b312ed15bfbe391f
Dan Ford <Dan@Fordsville.co.uk>
2016-07-15 19:50:47 +0100

    Add project files.

---
+++ Jam/commands.js
@@ -0,0 +1,114 @@
+﻿module.exports = {
+    "ping": {
+        usages: [
+            []
+        ],
+        description: "Replies with \"Pong!\", good for testing bot responsiveness.",
+        process: function(client, message, suffix)
+        {
+            client.reply(message, "Pong!");
+        }
+    },
+    "servers": {
+        usages: [
+            []
+        ],
+        description: "Lists the servers the bot is connected to.",
+        process: function(client, message, suffix)
+        {
+            client.sendMessage(message.channel, client.servers);
+        }
+    },
+    "channels": {
+        usages: [
+            []
+        ],
+        description: "Lists the channels the bot is watching",
+        process: function(client, message, suffix)
+        {
+            client.sendMessage(message.channel, client.channels)
+        }
+    },
+    "myid": {
+        usages: [
+            []
+        ],
+        description: "Shows the User ID of the sender",
+        process: function(client, message, suffix)
+        {
+            client.sendMessage(message.channel, message.author.id);
+        }
+    },
+    "idle": {
+        usages: [
+            []
+        ],
+        description: "Sets the bot's status to Idle",
+        process: function(client, message, suffix)
+        {
+            client.setStatusIdle();
+        }
+    },
+    "online": {
+        usages: [
+            []
+        ],
+        description: "Sets the bot's status to Online",
+        process: function(client, message, suffix)
+        {
+            client.setStatusOnline();
+        }
+    },
+    "say": {
+        usages: ["message"],
+        description: "Bot says the given message",
+        process: function(client, message, suffix)
+        {
+            client.sendMessage(message.channel, suffix);
+        }
+    },
+    /*"wolfram": {
+        usages: [
+        []
+        ],
+        description: "Gives results from WolframAlpha using the given search terms.",
+        requiresAdmin: false,
+        process: function (client, message, suffix) {
+            if (!suffix) {
+                bot.sendMessage(msg.channel, "Usage: !wolfram <search terms> (Ex. !wolfram integrate 4x)");
+                return;
+            }
+            wolfram_plugin.respond(suffix, msg.channel, bot);
+        }
+    },*/
+    "tts": {
+        usages: [
+            []
+        ],
+        description: "The bot says the message with Text to Speech",
+        process: function(client, message, suffix)
+        {
+            client.sendMessage(message.channel, suffix, { tts: true });
+        }
+    },
+    "permissions": {
+        usages: [
+            ["action"],
+            ["action", "command", "target", "value"]
+        ],
+        description: "",
+        process: function(client, message, params)
+        {
+
+        }
+    },
+    "": {
+        usages: [
+            []
+        ],
+        description: "",
+        process: function(client, message, suffix)
+        {
+        }
+    }
+};
---
+++ Jam/permissions.js
@@ -0,0 +1,65 @@
+var p = {}
+try {
+	p.permissions = require("./permissions.json");
+} catch (e) {
+	throw "Cannot find \"permissions.json\"";
+}
+
+p.userHasPermission = function (user, server, command) {
+    var userRoles = server.getUserRoles(user);
+
+    if (!p.serverExists(server))
+        return false;
+
+    if (!p.commandExistsForServer(server,command))
+        return false;
+
+    if (p.userDefinedForCommand(command, user))
+        return p.permissions[server.id][command][users][user.id];
+
+    var highestRole = undefined;
+    for (var role in userRoles) {
+        if (!p.roleDefinedForCommand(command, role))
+            continue;
+
+        if (highestRole == undefined)
+            highestRole = role;
+        else if (role.position > highestRole.position)
+            highestRole = role;
+    }
+    if (highestRole != undefined)
+        return p.permissions[server.id][command][roles][highestRole.id];
+
+    if (p.permissions[server.id][command].hasOwnProperty("all"))
+        return p.permissions[server.id][command]["all"];
+
+    return false;
+};
+
+p.serverExists = function(server) {
+    return p.permissions.hasOwnProperty(server.id)
+}
+
+p.commandExistsForServer = function(server, command) {
+    return p.serverExists(server) ? 
+    	p.permissions[server.id].hasOwnProperty(command)
+    	: false;
+}
+
+p.userDefinedForCommand = function(server, command, user) {
+	return p.commandExistsForServer(server, command) ?
+		p.permissions[server.id][command][users].hasOwnProperty(user.id) 
+		: false;
+}
+
+p.roleDefinedForCommand = function(server, command, role) {
+	return p.commandExistsForServer(server, command) ?
+		p.permissions[server.id][command][roles].hasOwnProperty(role.id)
+		: false;
+}
+
+p.addPermission = function(params) {
+	if params.hasOwnProperty()
+}
+
+module.exports = p;

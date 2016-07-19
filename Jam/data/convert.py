23956966b251eb3bd1c65b8303410218642338f5
Guido "Harb <Harbingerofme@gmail.com>
2016-07-19 11:44:52 +0200

    fixed hanging and closing issue

--- Jam/data/convert.py
+++ Jam/data/convert.py
@@ -2,10 +2,9 @@ import sys
 f = open(sys.argv[1])
 str = f.read()
 import re
-str = '{'+str[1:len(str)-2]+'}'
+str = '{'+str[1:len(str)-1]+'}'
 str = re.sub(r'{"id":("[^"]+"),',r'\1:{',str)
 f.close()
 f = open(sys.argv[1],'w')
 f.write(str)
 f.close()
-raw_input("")

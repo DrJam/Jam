0ae40b44226923dd47a2f1ce271903fd11f65c26
Dan Ford <Dan@Fordsville.co.uk>
2016-08-22 23:35:33 +0100

    formatting

--- modules/games.js
+++ modules/games.js
@@ -2,14 +2,21 @@
 
 games.defaultTime = Date.now();
 
-games.Update = function(user,data)
-{
+games.Update = function (user, data) {
     if (!user.bot) {
-        if (games.UserList[user.id] == undefined) { games.UserList[user.id] = games.defaultTime; }          //assert the time data exists.
+        if (games.UserList[user.id] == undefined) {
+             games.UserList[user.id] = games.defaultTime; 
+        }
         if (user.game != null && user.game.url == undefined) {
-            if (data.games[user.game.name] == undefined) { data.games[user.game.name] = 0; }                //assert the game data exists.
-            if (data.users[user.id] == undefined) { data.users[user.id] = {}; }                             //assert the user data exists.
-            if (data.users[user.id][user.game.name] == undefined) { data.users[user.id][user.game.name] = 0; }  //assert the user game data exists.
+            if (data.games[user.game.name] == undefined) {
+                data.games[user.game.name] = 0;
+            }
+            if (data.users[user.id] == undefined) {
+                data.users[user.id] = {};
+            }
+            if (data.users[user.id][user.game.name] == undefined) {
+                data.users[user.id][user.game.name] = 0;
+            }
             data.users[user.id][user.game.name] += Date.now() - games.UserList[user.id];
             data.games[user.game.name] += Date.now() - games.UserList[user.id];
         }

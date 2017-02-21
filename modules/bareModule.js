3678e768d09a0d555683e30a812f05be5a9e0b0f
DrJam <Dan@Fordsville.co.uk>
2017-02-21 16:08:34 +0000

    rename

--- modules/bareModule.js
+++ modules/bareModule.js
@@ -1,11 +1,11 @@
-var MODULE = function(){}//constructor, use this only to initiliase variables NEEDED to be initiliased before onReady. Note that the onReady might not occur before severl other modules have fired their onReady.
-MODULE.prototype.Name = "baremodule";//string
-MODULE.prototype.Dependencies = [];//strings
-MODULE.prototype.Events = []//OPTIONAL, objects of format {type:string,method:functionAsString}
-MODULE.prototype.Data = null;//OPTIONAL, string naming the collection module wishes to access
-MODULE.prototype.Config = [];//OPTIONAL, objects of type {name:attribute}, will been have been expanded to {name:attribute,value:value} by the time onReady hits.
-MODULE.prototype.onReady = function(pars){//The real constructor.
+var bareModule = function(){}//constructor, use this only to initiliase variables NEEDED to be initiliased before onReady. Note that the onReady might not occur before severl other modules have fired their onReady.
+bareModule.prototype.Name = "baremodule";//string
+bareModule.prototype.Dependencies = [];//strings
+bareModule.prototype.Events = []//OPTIONAL, objects of format {type:string,method:functionAsString}
+bareModule.prototype.Data = null;//OPTIONAL, string naming the collection module wishes to access
+bareModule.prototype.Config = [];//OPTIONAL, objects of type {name:attribute}, will been have been expanded to {name:attribute,value:value} by the time onReady hits.
+bareModule.prototype.onReady = function(pars){//The real constructor.
 }   
 
 
-module.exports = new MODULE();
+module.exports = new bareModule();

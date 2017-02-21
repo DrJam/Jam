a7d2eb7650960e3d50963805b297fcfb314412c5
Guido "Harb <Harbingerofme@gmail.com>
2017-02-22 00:26:42 +0100

    Updated the comments to make slightly more sense.

--- modules/bareModule.js
+++ modules/bareModule.js
@@ -1,10 +1,10 @@
-var MODULE = function(){}//constructor, use this only to initiliase variables NEEDED to be initiliased before onReady. Note that the onReady might not occur before severl other modules have fired their onReady.
+var MODULE = function(){}//constructor, use this only to initialise variables NEEDED to be initialised before onReady. Note that the onReady might not occur before severl other modules have fired their onReady.
 MODULE.prototype.Name = "baremodule";//string
 MODULE.prototype.Dependencies = [];//strings
 MODULE.prototype.Events = []//OPTIONAL, objects of format {type:string,method:functionAsString}
 MODULE.prototype.Data = null;//OPTIONAL, string naming the collection module wishes to access
 MODULE.prototype.Config = [];//OPTIONAL, objects of type {name:attribute}, will been have been expanded to {name:attribute,value:value} by the time onReady hits.
-MODULE.prototype.onReady = function(pars){//The real constructor.
+MODULE.prototype.onReady = function(pars){//The preferred constructor.
 }   
 
 
